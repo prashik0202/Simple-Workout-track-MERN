39d8c8159c41bfca87075ac2b34b6cafcc21897f
Prashik <gamreprashik03@gmail.com>
2023-03-12 11:19:27 +0530

    signup handled

--- README.md
+++
@@ -1,8 +0,0 @@
-# Simple-Workout-track-MERN
-This is simple MERN stack application.
-
-# ScreenShots:
-
-![image](https://user-images.githubusercontent.com/88095936/223319438-35a89be4-2ffc-43a8-a501-46012002ae5d.png)
-
- Note : Soon we will add Auth to each user and deploy on heroku
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // pages & components
 import Home from './pages/Home'
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import Navbar from './components/Navbar'
 
 function App() {
@@ -16,7 +18,16 @@ function App() {
               path="/" 
               element={<Home />} 
             />
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
+            />
           </Routes>
+          
         </div>
       </BrowserRouter>
     </div>
